e422b765f84ace246099d3a287a4784008c8d0fe
Deke310 <135410011+Deke206@users.noreply.github.com>
2026-08-05 15:57:56 -0700

    Add lead endpoint configuration

---
+++ js/contact-lead-config.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+
+  window.ShynetymeLeadConfig = Object.freeze({
+    // Paste the deployed Google Apps Script /exec URL here before merging this branch.
+    endpoint: "",
+    fallbackEmail: "westsidelistingservices@gmail.com",
+    timeoutMs: 25000
+  });
+})();
